3b796ffdde6ff26dcb9954d1fb679b221fe14d2b
Veersaxena2008 <pooja.22177@gmail.com>
2020-12-28 14:47:39 +0530

    adding angry bird work done so far

---
+++ Bird.js
@@ -0,0 +1,14 @@
+class Bird extends BaseClass{
+    constructor(x, y) {
+     super(x, y, 50, 50) // is the constructor of parent class
+      this.image = loadImage("sprites/bird.png");
+     
+    };
+    display(){
+      super.display(); // accessing parent's display
+      var pos = this.body.position;
+      pos.x = mouseX;
+      pos.y = mouseY;
+    };
+  };
+  
