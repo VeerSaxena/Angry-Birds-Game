a051360849cdf5fca1eb4fcbf76cad1e5f16c06f
Veersaxena2008 <pooja.22177@gmail.com>
2021-01-06 15:06:30 +0530

    added catapult and slingshot

--- slingshot.js
+++ slingshot.js
@@ -9,6 +9,10 @@ class Slingshot{
         }
 
         this.sling = Matter.Constraint.create(options);
+        this.sling1 = loadImage("sprites/sling1.png");
+        this.sling2 = loadImage("sprites/sling2.png");
+        this.sling3 = loadImage("sprites/sling3.png");
+
         World.add(world, this.sling);   
     }
 
@@ -17,17 +21,34 @@ class Slingshot{
     }
 
     display(){
+        image(this.sling1, 200, 22);
+        image(this.sling2, 170, 22);
+
         if(this.sling.bodyA){
 
         var pointA = this.sling.bodyA.position; // bird body
         var pointB = this.sling.pointB;
 
         push ();
+        stroke (66, 39, 15);
         //line between two body (x, y)
         //line(x1, y1, x2, y2)
-        strokeWeight (4);
-        fill ("brown");
-        line (pointA.x, pointA.y, pointB.x, pointB.y);
+        
+        if(pointA.x<220){
+         strokeWeight (7);
+       
+        line (pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+        line (pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+        image(this.sling3, pointA.x-35, pointA.y-10, 15, 30);
+
+        }
+        else{
+            strokeWeight (3);
+       
+        line (pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+        line (pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+        image(this.sling3, pointA.x-35, pointA.y-10, 15, 30);
+        }
         pop ();
     }
     }
